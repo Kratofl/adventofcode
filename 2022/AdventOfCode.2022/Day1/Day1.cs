a7dc0cb3354408c3465427abefdf03ed3e2ebbdc
Kratofl <l.achler@mopsoft.de>
2022-11-30 16:37:15 +0100

    Init 2022

---
+++ 2022/AdventOfCode.2022/Day1/Day1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022.Day1
+{
+    public static class Day1
+    {
+        private static string[] _input = File.ReadAllLines("/Input.csv");
+
+        public static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
---
+++ 2022/AdventOfCode.2022/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Select a day and the part");
+Console.WriteLine("start -d <day> -p <part>");
+string inputCmd = Console.ReadLine();
+
+Console.WriteLine();
