531e29361e7344a0fa51e14a160abdd5f0b47baa
Kratofl <l.achler@mopsoft.de>
2022-12-01 09:19:25 +0100

    Solved Day 1

--- 2022/Day1/Day1.ps1
+++ 2022/Day1/Day1.ps1
@@ -2,23 +2,46 @@
 
 function DayOne-PartOne {
     [CmdletBinding()]
-    param (
-        
-    )   
+    param ()   
     process {
+        [int] $mostcalories = 0;
+
+        [int] $currentElvCalories = 0;
+        foreach ($line in $lines) {
+            # Elv is done
+            if ([string]::IsNullOrEmpty($line)) {
+                if ($currentElvCalories -gt $mostcalories) {
+                    $mostcalories = $currentElvCalories
+                }
+                $currentElvCalories = 0;
+                continue;
+            }
+
+            $currentElvCalories += [int]$line;
+        }
+        Write-Host $mostcalories
     }   
-    end {
-        
-    }
 }
 function DayOne-PartTwo {
     [CmdletBinding()]
-    param (
-        
-    )
+    param ()
     process {
+        $mostcalories = New-Object int[] 0;
+
+        [int] $currentElvCalories = 0;
+        foreach ($line in $lines) {
+            # Elv is done
+            if ([string]::IsNullOrEmpty($line)) {
+                $mostcalories += $currentElvCalories
+                $currentElvCalories = 0;
+                continue;
+            }
+            $currentElvCalories += [int]$line;
+        }
+        $sortedCalories = $mostcalories | Sort-Object -Descending
+        $selectedCalories = $sortedCalories | Select-Object -First 3 -Wait
+        $output = $selectedCalories[0] + $selectedCalories[1] + $selectedCalories[2];
+        Write-Host $output;
     }   
-    end {
-        
-    }
+    end {}
 }
